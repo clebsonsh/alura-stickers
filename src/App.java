4554036ea7502a70c7cccb5710d3163b5399e487
Clebson Moura <clebson.sh@gmail.com>
2022-07-18 10:43:04 -0300

    AULA 01 - CONSUMINDO UMA API DE FILMES COM JAVA

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Fazer um conexão HTTP e buscar os topp 250 filmes
+
+        // Gerar uma URI como o path da API + a API_KEY
+        String API_KEY = "k_0az0z52e";
+        String url = "https://imdb-api.com/en/API/Top250Movies/";
+        URI endereco = URI.create(url + API_KEY);
+
+        // Faz o GET dos dados da API
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // Monstra os dados retornados da API no console
+        // System.out.println(body);
+
+        // Extrair só os dados que interessam (titulo, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // Exibir e manipular os dados
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+
     }
 }
